f711e482921c47560dc2d348d213006580078093
shshalom <shalom@nuro.im>
2021-04-08 16:12:00 +0300

    Initial Commit

---
+++ AutoDeskTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoDeskTodo
+//
+//  Created by Shalom Shwaitzer on 08/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
